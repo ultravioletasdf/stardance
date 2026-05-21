37ab3ed2f2655a0875780749bf8033bf21b5a5b9
Kartikey Chauhan <me@cskartikey.dev>
2026-05-21 20:58:05 +0530

    fix(project): add hackatime_project_ids to project_params

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -320,7 +320,7 @@ class ProjectsController < ApplicationController
   end
 
   def project_params
-    params.require(:project).permit(:title, :description, :demo_url, :repo_url, :readme_url, :banner, :ai_declaration)
+    params.require(:project).permit(:title, :description, :demo_url, :repo_url, :readme_url, :banner, :ai_declaration, hackatime_project_ids: [])
   end
 
   def hackatime_project_ids
